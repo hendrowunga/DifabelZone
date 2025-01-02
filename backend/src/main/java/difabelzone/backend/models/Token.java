501e8b1de029cd2bd261ecba08a8c2708457a862
hendrowunga <hendrowunga073@gmail.com>
2025-01-02 20:41:36 +0700

    #2-develop-backend implements  model Token

---
+++ backend/src/main/java/difabelzone/backend/models/Token.java
@@ -0,0 +1,32 @@
+package difabelzone.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
