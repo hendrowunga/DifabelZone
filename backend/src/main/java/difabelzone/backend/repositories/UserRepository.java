307796c909f8a946a559dd852fe5778e8b7acc4a
hendrowunga <hendrowunga073@gmail.com>
2025-01-02 20:40:53 +0700

    #2-develop-backend implements  UserRepository

--- backend/src/main/java/difabelzone/backend/repositories/UserRepository.java
+++ backend/src/main/java/difabelzone/backend/repositories/UserRepository.java
@@ -2,12 +2,17 @@ package difabelzone.backend.repositories;
 
 import difabelzone.backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-//import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-//    Optional<User> findByEmail(String email);
+    @Query("SELECT u FROM User u JOIN FETCH u.addresses a WHERE a.addressId = ?1")
+    List<User> findByAddress(Long addressId);
+
+    Optional<User> findByEmail(String email);
 }
