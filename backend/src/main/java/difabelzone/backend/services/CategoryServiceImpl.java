4b7cb1c048591702479d4bea6a732157593c4c8d
hendrowunga <hendrowunga073@gmail.com>
2025-01-04 07:02:45 +0700

    #2-develop-backend implements services to CategoryServiceImpl

---
+++ backend/src/main/java/difabelzone/backend/services/CategoryServiceImpl.java
@@ -0,0 +1,38 @@
+package difabelzone.backend.services;
+
+import difabelzone.backend.models.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private List<Category> categories = new ArrayList<>();
+    private Long nextId=1L;
+
+    @Override
+    public List<Category> getAllCategories() {
+
+        return categories;
+    }
+
+    @Override
+    public void createCategories(Category category) {
+        category.setCategoryId(nextId++);
+        categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category=categories.stream()
+                .filter(c->c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if(category==null){
+            return "Category not found";
+        }
+        categories.remove(category);
+        return "Category with categoryId " + categoryId + " delete successfully ";
+    }
+}
