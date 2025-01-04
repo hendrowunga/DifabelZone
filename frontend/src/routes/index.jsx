752df89673909d730282392d4b2aa9b9806bb8a1
riooocn <davidriochristian43@gmail.com>
2025-01-04 12:44:03 +0700

    membuat Routes

---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
+
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import DashboardPage from "../pages/DashboardPage";
+import RegisterPage from "../pages/RegisterPage";
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <div>Routes Not Found!</div>,
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
+const AppRouter = () => {
+  return (
+    <>
+      <Toaster position="top-center" richColors />
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default AppRouter;
