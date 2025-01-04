eb167cb72122440f2aa0ebbce04841ad19d1aa4d
hendrowunga <hendrowunga073@gmail.com>
2025-01-04 08:07:07 +0700

    #2-develop-backend Introduce update operation in CategoryController to enable secure and efficient handling of category update requests

--- backend/src/main/java/difabelzone/backend/controllers/CategoryController.java
+++ backend/src/main/java/difabelzone/backend/controllers/CategoryController.java
@@ -39,14 +39,23 @@ public class CategoryController {
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
 
         try {
-            String status = categoryService.deleteCategory(categoryId);
+            String status = categoryService.deleteCategories(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
 //            return ResponseEntity.ok(status);
 //            return ResponseEntity.status(HttpStatus.OK).body(status);
         } catch (ResponseStatusException e) {
             return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
 
+    @PutMapping("/public/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category,@PathVariable Long categoryId) {
+        try {
+            Category savedCategory=categoryService.updateCategories(category,categoryId);
+            return new ResponseEntity<>("Category with Category id: " + categoryId ,HttpStatus.OK);
 
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
         }
     }
 }
