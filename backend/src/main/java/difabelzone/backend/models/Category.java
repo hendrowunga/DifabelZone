1813860542b257693df1549fe6d20b0bcd80f8ce
hendrowunga <hendrowunga073@gmail.com>
2025-01-05 08:14:34 +0700

    #2-develop-backend Introduce the Category model implementation to define the entity structure and attributes

--- backend/src/main/java/difabelzone/backend/models/Category.java
+++ backend/src/main/java/difabelzone/backend/models/Category.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class Category {
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
     @NotBlank
