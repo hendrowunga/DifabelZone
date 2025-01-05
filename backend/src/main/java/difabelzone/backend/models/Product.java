c40e6d05bfd629681592e64751b1633cb9e130f1
hendrowunga <hendrowunga073@gmail.com>
2025-01-06 03:08:09 +0700

    #2-develop-backend Add Product model implementation
    Implemented the Product model to represent product data in the backend.
    This serves as a foundational entity for product-related operations and database interactions.

---
+++ backend/src/main/java/difabelzone/backend/models/Product.java
@@ -0,0 +1,41 @@
+package difabelzone.backend.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain at least 3 characters")
+    private String productName;
+    private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain at least 6 characters")
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+}
