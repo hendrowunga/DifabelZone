b28f0782c1945168a7835f85b2d0476df471099c
hendrowunga <hendrowunga073@gmail.com>
2025-01-07 01:13:07 +0700

    #2-develop-backend Add Role Model for User Role Management Introduced Role model to define and store user roles within the application.

--- backend/src/main/java/difabelzone/backend/models/Role.java
+++ backend/src/main/java/difabelzone/backend/models/Role.java
@@ -1,6 +1,26 @@
 package difabelzone.backend.models;
 
-public enum Role {
-    USER,
-    ADMIN
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "role_id")
+     private Integer roleId;
+
+     @Enumerated(EnumType.STRING)
+     @Column(length = 20, name = "role_name")
+     private AppRole roleName;
+
+     public Role(AppRole roleName) {
+          this.roleName = roleName;
+     }
 }
+
