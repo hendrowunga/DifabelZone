8ac908ca6b779c3180a1f6934ebfe75ff29daf0b
hendrowunga <hendrowunga073@gmail.com>
2025-01-07 01:12:38 +0700

    #2-develop-backend Add UserRepository for User Entity Management
    Implemented UserRepository to manage CRUD operations for user data in the database.

--- backend/src/main/java/difabelzone/backend/repositories/UserRepository.java
+++ backend/src/main/java/difabelzone/backend/repositories/UserRepository.java
@@ -2,17 +2,17 @@ package difabelzone.backend.repositories;
 
 import difabelzone.backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u JOIN FETCH u.addresses a WHERE a.addressId = ?1")
-    List<User> findByAddress(Long addressId);
+     Optional<User> findByUserName(String username);
 
-    Optional<User> findByEmail(String email);
+     Boolean existsByUserName(String username);
+
+     Boolean existsByEmail(String email);
 }
+
