b56e9b6c5b4dba3b2f1cc455de2499bf70770605
hendrowunga <hendrowunga073@gmail.com>
2025-01-08 06:47:14 +0700

    #2-develop-backend Introduce AddressRepository for data access implementation
    Added AddressRepository to handle database operations for address-related entities.
    This improves data persistence layer and simplifies database interaction.

--- backend/src/main/java/difabelzone/backend/repositories/AddressRepository.java
+++ backend/src/main/java/difabelzone/backend/repositories/AddressRepository.java
@@ -3,8 +3,7 @@ package difabelzone.backend.repositories;
 import difabelzone.backend.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressRepository extends JpaRepository<Address,Long> {
 
-    Address findByVillageAndDistrictAndSubDistrictAndZipcodeAndStreetAndProvince(String street, String zip_code, String village,
-                                                                           String sub_district, String district, String province);
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
 }
