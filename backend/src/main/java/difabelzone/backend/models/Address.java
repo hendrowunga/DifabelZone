c7d82abfcbc1ce47e3499fd1e96404b810a1a7d8
hendrowunga <hendrowunga073@gmail.com>
2025-01-08 06:45:29 +0700

    Models: #2-develop-backend Add Address model implementation
    Implemented the Address model to represent address data in the backend.
    This serves as a foundational entity for address-related operations and database interactions.

--- backend/src/main/java/difabelzone/backend/models/Address.java
+++ backend/src/main/java/difabelzone/backend/models/Address.java
@@ -2,6 +2,7 @@ package difabelzone.backend.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,28 +11,60 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-@Table(name = "address")
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotBlank
-    private String street;
-    @NotBlank
-    private String zip_code;
-    @NotBlank
-    private String village;
-    @NotBlank
-    private String sub_district;
-    @NotBlank
-    private String district;
-    @NotBlank
-    private String province;
-    @ManyToMany(mappedBy = "addresses")
-    private List<User> users=new ArrayList<>();
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long addressId;
+
+     @NotBlank
+     @Size(min = 5, message = "Street name must be atleast 5 characters")
+     private String street;
+
+     @NotBlank
+     @Size(min = 5, message = "Building name must be atleast 5 characters")
+     private String buildingName;
+
+     @NotBlank
+     @Size(min = 3, message = "Village name must be atleast 3 characters")
+     private String village;
+
+     @NotBlank
+     @Size(min = 4, message = "Sub-District name must be atleast 4 characters")
+     private String subdistrict;
+
+     @NotBlank
+     @Size(min = 4, message = "City name must be atleast 4 characters")
+     private String city;
+
+     @NotBlank
+     @Size(min = 4, message = "Province name must be atleast 4 characters")
+     private String province;
+
+     @NotBlank
+     @Size(min = 5, message = "Pincode must be atleast 5 characters")
+     private String pincode;
+
+     public Address(String street, String buildingName, String village, String subdistrict, String city, String province, String pincode) {
+          this.street = street;
+          this.buildingName = buildingName;
+          this.village = village;
+          this.subdistrict = subdistrict;
+          this.city = city;
+          this.province = province;
+          this.pincode = pincode;
+     }
+
+     @ManyToOne
+     @JoinColumn(name = "user_id")
+     private User user;
+
+
 }
+
+
+
