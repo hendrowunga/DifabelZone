99dbf52d18848db25ed7d8cd9e675e7d9b671742
hendrowunga <hendrowunga073@gmail.com>
2025-01-08 07:06:13 +0700

    #2-develop-backend Add Product model implementation
    Implemented the Product model to represent product data in the backend.
    This serves as a foundational entity for product-related operations and database interactions.

--- backend/src/main/java/difabelzone/backend/models/Product.java
+++ backend/src/main/java/difabelzone/backend/models/Product.java
@@ -10,6 +10,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -40,5 +43,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "seller_id")
     private User user;
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<CartItem> products = new ArrayList<>();
 
 }
