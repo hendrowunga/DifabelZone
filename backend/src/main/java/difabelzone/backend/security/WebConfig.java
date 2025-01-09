7ddc1c2dcd89e8b5f676b91af10573e46259b12b
hendrowunga <hendrowunga073@gmail.com>
2025-01-09 08:49:10 +0700

    #2-develop-backend feat: Introduce WebSecurityConfig for enhanced security implementation
                       - Added WebSecurityConfig class to configure Spring Security.
                       - Implemented basic authentication and authorization setup.
                       - Defined security rules for public and protected endpoints.
                       - Applied CSRF protection and session management.
    
                       This change improves the overall security of the backend by centralizing security configurations.

---
+++ backend/src/main/java/difabelzone/backend/security/WebConfig.java
@@ -0,0 +1,22 @@
+package difabelzone.backend.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+     @Value("${spring.application.frontend.url}")
+     String frontEndUrl;
+
+     @Override
+     public void addCorsMappings(CorsRegistry registry) {
+          registry.addMapping("/**")
+                  .allowedOrigins("http://localhost:3000", frontEndUrl)
+                  .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                  .allowedHeaders("*")
+                  .allowCredentials(true);
+     }
+}
